ccc205b1a870f7c7c95fac075cad3e12ab9a705b
Michael Prat <michaelandgobaw@gmail.com>
2018-02-27 11:52:41 +0700

    Hasil pertama

---
+++ Hari06latihan04.php
@@ -0,0 +1,9 @@
+<?php
+$sisi=5;
+$sisi=$sisi*$sisi;
+echo"Luas Persegi panjang dengan sisi 5 =".$sisi."<br></br>";
+$sisi=5;
+$sisi++;
+$sisi=$sisi*$sisi;
+echo"Jika sisi ditambahkan incerement 1 maka luasnya =".$sisi."<br></br>";
+?>
